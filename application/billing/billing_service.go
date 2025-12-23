11f32553adb8664153a8fe17c16b342118313f1a
Valentina Arias <vaariasa@unal.edu.co>
2025-12-23 15:40:28 -0500

    feat: send payment_confirm event when billing is approved

--- application/billing/billing_service.go
+++ application/billing/billing_service.go
@@ -19,7 +19,7 @@ import (
 )
 
 type BillingService interface {
-	CreateBilling(ctx context.Context, provider, posId, customerId string, customer *domain.Customer) (*domain.Billing, error)
+	CreateBilling(ctx context.Context, provider, posId, customerId, userId string, customer *domain.Customer) (*domain.Billing, error)
 	GetAllBillings(ctx context.Context) ([]domain.Billing, error)
 	GetBillingById(ctx context.Context, id string) (*domain.Billing, error)
 	GetBillingByIdWithExternalData(ctx context.Context, id string) (*domain.Billing, error)
@@ -60,7 +60,7 @@ func NewBillingService(
 	}
 }
 
-func (s *billingService) CreateBilling(ctx context.Context, provider, posId, customerId string, customer *domain.Customer) (*domain.Billing, error) {
+func (s *billingService) CreateBilling(ctx context.Context, provider, posId, customerId, userId string, customer *domain.Customer) (*domain.Billing, error) {
 	invoiceConfig := billingHelpers.GetInvoiceConfigFromEnv()
 	billing, err := s.billingFactory.CreateBilling(provider, posId, customer, invoiceConfig)
 	if err != nil {
@@ -68,6 +68,7 @@ func (s *billingService) CreateBilling(ctx context.Context, provider, posId, cus
 	}
 
 	billing.CustomerId = &customerId
+	billing.UserId =&userId
 
 	err = s.repo.SaveBilling(ctx, billing)
 	if err != nil {
